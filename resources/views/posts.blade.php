192ddb10d5fe4d6b5e8402e36018459eb2c36566
dfilter <dirk_mail@protonmail.com>
2021-10-18 09:39:36 -0400

    Store blog posts ans html

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel varius enim, vel fringilla mi. Cras nunc dui,
+            posuere sed placerat vel, faucibus id purus. Suspendisse ultricies ante ut gravida sodales. Phasellus augue
+            augue, bibendum non mollis at, blandit condimentum elit. Morbi dapibus felis ut tincidunt placerat. Vestibulum
+            ipsum odio, tincidunt sed nisl at, convallis congue arcu. Integer in fringilla velit. Aenean suscipit commodo
+            neque, sit amet egestas risus maximus id. Nam molestie nulla eget dui fermentum, vitae mollis arcu eleifend.
+            Donec malesuada ut ipsum id tincidunt.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel varius enim, vel fringilla mi. Cras nunc dui,
+            posuere sed placerat vel, faucibus id purus. Suspendisse ultricies ante ut gravida sodales. Phasellus augue
+            augue, bibendum non mollis at, blandit condimentum elit. Morbi dapibus felis ut tincidunt placerat. Vestibulum
+            ipsum odio, tincidunt sed nisl at, convallis congue arcu. Integer in fringilla velit. Aenean suscipit commodo
+            neque, sit amet egestas risus maximus id. Nam molestie nulla eget dui fermentum, vitae mollis arcu eleifend.
+            Donec malesuada ut ipsum id tincidunt.
+        </p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel varius enim, vel fringilla mi. Cras nunc dui,
+            posuere sed plac
+            augue, bibendum non mollis at, blandit condimentum elit. Morbi dapibus felis ut tincidunt placerat. Vestibulum
+            ipsum odio, tincidunt sed nisl at, convallis congue arcu. Integer in fringilla velit. Aenean suscipit commodo
+            neque, sit amet egestas risus maximus id. Nam molestie nulla eget dui fermentum, vitae mollis arcu eleifend.
+            Donec malesuada ut ipsum id tincidunt.
+        </p>
+    </article>
+</body>
+</html>
