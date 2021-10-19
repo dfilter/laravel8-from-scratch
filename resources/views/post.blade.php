2862942ad15409ee1e2740ab03d2b6e7eb625718
dfilter <dirk_mail@protonmail.com>
2021-10-19 09:58:26 -0400

    Blade Layouts Two Ways

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>My Blog</title>
-        <link rel="stylesheet" href="/app.css">
-    </head>
-    <body>
-        <article>
-            <h1>{{ $post->title }}</h1>
-            <idv>
-                {!! $post->body !!}
-            </idv>
-        </article>
+<x-layout>
+    <article>
+        <h1>{{ $post->title }}</h1>
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
 
-        <a href="/">Go Back</a>
-    </body>
-</html>
+    <a href="/">Go Back</a>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+<x-layout>
     @foreach ($posts as $post)
         <article>
             <h1>
                 <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title; }}
+                    {{ $post->title }}
                 </a>
             </h1>
             <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
-</body>
-</html>
+</x-layout>
